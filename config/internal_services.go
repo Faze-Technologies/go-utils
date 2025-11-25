b9a86615c32d581dd38272134f8ab61345be8409
chirayu-fancraze <178133081+chirayu-fancraze@users.noreply.github.com>
2025-11-25 13:12:24 +0530

    changes

--- config/internal_services.go
+++ config/internal_services.go
@@ -63,8 +63,9 @@ var InternalServices = map[string]string{
 	"cmTradeService":                   "http://cm-trade-service.cm-trade-service.svc.cluster.local",
 	"finopsAdminService":               "http://finops-admin-service.finops-admin-service.svc.cluster.local",
 	"openApiAuthService":               "http://openapi-auth-service.openapi-auth-service.svc.cluster.local",
-	"superteamScrapService":           "http://superteam-scrap-service.superteam-scrap-service.svc.cluster.local",
-	"superteamUpgradeService":"http://superteam-upgrade-service.superteam-upgrade-service.svc.cluster.local",
+	"superteamScrapService":            "http://superteam-scrap-service.superteam-scrap-service.svc.cluster.local",
+	"superteamUpgradeService":          "http://superteam-upgrade-service.superteam-upgrade-service.svc.cluster.local",
+	"editionUpgradeService":            "http://edition-upgrade-service.edition-upgrade-service.svc.cluster.local",
 }
 
 // ProxyServices generates the proxy URLs if needed
