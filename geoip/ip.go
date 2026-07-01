a949d503993beb9098614de0fa9912565219c66c
AAKARSh-fancraze <aakarsh@fancraze.com>
2026-07-01 20:13:21 +0530

    too much fallbacks added

--- geoip/ip.go
+++ geoip/ip.go
@@ -17,7 +17,32 @@ func normalizeIP(s string) string {
 // writes its own, so the leftmost entry is the real client. If you ever move
 // off GCP (e.g. add Cloudflare), revisit this.
 func ResolveClientIP(h http.Header, remoteAddr string) string {
-	if xff := h.Get("X-Forwarded-For"); xff != "" {
+	var xff string
+
+	for _, key := range []string{
+		"X-Forwarded-For",
+		"X-Forwarded-for",
+		"X-forwarded-For",
+		"X-forwarded-for",
+		"x-Forwarded-For",
+		"x-Forwarded-for",
+		"x-forwarded-For",
+		"x-forwarded-for",
+
+		"X_Forwarded_For",
+		"X_Forwarded_for",
+		"X_forwarded_For",
+		"X_forwarded_for",
+		"x_Forwarded_For",
+		"x_Forwarded_for",
+		"x_forwarded_For",
+		"x_forwarded_for",
+	} {
+		if xff = h.Get(key); xff != "" {
+			break
+		}
+	}
+	if xff != "" {
 		first := normalizeIP(strings.Split(xff, ",")[0])
 		if ip := net.ParseIP(first); ip != nil {
 			return ip.String()
